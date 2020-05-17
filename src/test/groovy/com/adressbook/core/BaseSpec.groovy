2907e7819a56a3daa97013d1c77d7f7922b39119
Jeevan.Adiga <adiga.jeevan@gmail.com>
2020-05-17 18:17:44 +0530

    Enable spock-report for report generation

--- src/test/groovy/com/adressbook/core/BaseSpec.groovy
+++ src/test/groovy/com/adressbook/core/BaseSpec.groovy
@@ -4,4 +4,9 @@ import geb.spock.GebSpec
 
 class BaseSpec extends GebSpec {
 
+    def setupSpec() {
+        reportHeader "Browser Name: ${driver.capabilities['browserName']}"
+        reportHeader "Platform: ${System.getProperty('os.name')}"
+        reportHeader "SessionId: ${driver.sessionId}"
+    }
 }
